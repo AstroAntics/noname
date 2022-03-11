efd61ca239b2179bd62b427b0002a2915a7eb087
SilverCrown <vucinicm@asbindia.org>
2022-03-12 01:40:26 +0530

    Adding files for noname

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    /*
+    * Hello world - printing to console
+    * Regular and positional arguments
+    */
+    println!("Hello, world!");
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    /*
+    * Software interrupt
+    */
+    // panic!("lol I broke something");
+
+
+
+
+
+}
